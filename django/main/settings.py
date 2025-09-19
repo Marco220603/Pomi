6cdb79be19e680693c94b31d8bc79fbb3d6faeda
Marco220603 <217613265+MarcoFerrey@users.noreply.github.com>
2025-09-19 03:19:59 -0500

    allowed host

--- django/main/settings.py
+++ django/main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-vmksyx4^=(#4p&5fv(+n6-k$zmn95y5uhxqu@@e3t0#rs^xso@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.46.10','52.15.79.134','172.31.39.137','18.188.98.100','172.31.42.94','3.21.129.53', 'localhost', '127.0.0.1', 'pomi-production.up.railway.app']
+ALLOWED_HOSTS = ['172.31.46.10','52.15.79.134','172.31.39.137','18.188.98.100','172.31.42.94','3.21.129.53', 'localhost', '127.0.0.1', 'pomi-production.up.railway.app', '18.188.98.100']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://pomi-production.up.railway.app",
