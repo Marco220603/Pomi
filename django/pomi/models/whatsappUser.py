6d9e0bd41d784e59493916051da79dd8f3962d61
Marco220603 <217613265+MarcoFerrey@users.noreply.github.com>
2025-11-07 00:19:03 -0500

    7/11.4

--- django/pomi/models/whatsappUser.py
+++ django/pomi/models/whatsappUser.py
@@ -5,3 +5,4 @@ class WhatsAppUserStudent(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, unique=False)
     last_date_update = models.DateTimeField(auto_now=True)
+# Texto Prueba
